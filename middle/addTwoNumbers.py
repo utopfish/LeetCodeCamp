176c8094349e9d534f2ce8170ecd7da01112dc11
utopfish <utopfish@163.com>
2019-10-27 14:15:33 +0800

    链表两数相加

---
+++ middle/addTwoNumbers.py
@@ -0,0 +1,48 @@
+# -*- coding=utf-8 -*-
+#@author:liuAmon
+#@contact:utopfish@163.com
+#@file:addTwoNumbers.py
+#@time: 2019/10/27 12:33
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        n = l1.val + l2.val
+        l3 = ListNode(n % 10)
+        l3.next = ListNode(n // 10)
+        p1 = l1.next
+        p2 = l2.next
+        p3 = l3
+        while True:
+            if p1 and p2:
+                sum = p1.val + p2.val + p3.next.val
+                p3.next.val = sum % 10
+                p3.next.next = ListNode(sum // 10)
+                p1 = p1.next
+                p2 = p2.next
+                p3 = p3.next
+            elif p1 and not p2:
+                sum = p1.val + p3.next.val
+                p3.next.val = sum % 10
+                p3.next.next = ListNode(sum // 10)
+                p1 = p1.next
+                p3 = p3.next
+            elif not p1 and p2:
+                sum = p2.val + p3.next.val
+                p3.next.val = sum % 10
+                p3.next.next = ListNode(sum // 10)
+                p2 = p2.next
+                p3 = p3.next
+            else:
+                if p3.next.val == 0:
+                    p3.next = None
+                break
+        return l3
+
+
+
