a7d980f4fff6da04dea7f241e59d76dfbbb72ddf
utopfish <utopfish@163.com>
2019-11-03 12:19:34 +0800

    二进制求和

---
+++ simple/addBinary.py
@@ -0,0 +1,32 @@
+# -*- coding=utf-8 -*-
+#@author:liuAmon
+#@contact:utopfish@163.com
+#@file:addBinary.py
+#@time: 2019/11/3 12:04
+class Solution2:
+    def addBinary(self, a: str, b: str) -> str:
+        #p记录进位
+        r, p = '', 0
+        d = len(b) - len(a)
+        a = '0' * d + a
+        b = '0' * -d + b
+        for i, j in zip(a[::-1], b[::-1]):
+            s = int(i) + int(j) + p
+            r = str(s % 2) + r
+            p = s // 2
+        return '1' + r if p else r
+class Solution:
+    def addBinary(self,a,b):
+        r,p='',0
+        d=len(a)-len(b)
+        a='0'*(-d)+a
+        b='0'*d+b
+        #[::-1]为数组逆序
+        for i,j in zip(a[::-1],b[::-1]):
+            s=int(i)+int(j)+p
+            r=str(s%2)+r
+            p=s//2
+        return '1'+r if p else r
+if __name__=="__main__":
+    Test=Solution()
+    print(Test.addBinary("11","1"))
