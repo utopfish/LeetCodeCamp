8365d912c2157df98bdd9f745e98191ec2ec9b01
utopfish <utopfish@163.com>
2019-11-14 14:59:13 +0800

    一个树的简单题，一会装clion，之后慢慢用c++来写

---
+++ simple/maxDepth.py
@@ -0,0 +1,20 @@
+#@Time:2019/11/14 14:58
+#@Author:liuAmon
+#@e-mail:utopfish@163.com
+#@File:maxDepth.py
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1
