6b2a3b91a0ef80e7af2b656ec613140a48112e2f
utopfish <utopfish@163.com>
2019-11-16 10:31:18 +0800

    一个简单动态规划的题，学姐今天过生日

--- simple/maxProfit.py
+++ simple/maxProfit.py
@@ -1,13 +1,16 @@
-# -*- coding=utf-8 -*-
-#@author:liuAmon
-#@contact:utopfish@163.com
-#@file:maxProfit.py
-#@time: 2019/11/6 21:54
+#@Time:2019/11/16 10:23
+#@Author:liuAmon
+#@e-mail:utopfish@163.com
+#@File:maxProfit.py
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pro=0
-        for i in range(1,len(prices)):
-            temp=prices[i]-prices[i-1]
-            if temp>0:pro+=temp
-        return pro
+        temp = max_profit = 0
+        for i in range(len(prices)-1):
+            temp = max(0, prices[i+1]-prices[i]+temp)
+            max_profit = max(max_profit, temp)
+        return max_profit
+
+if __name__=="__main__":
+    Test=Solution()
+    print(Test.maxProfit([7,1,5,3,6,4]))
---
+++ simple/maxProfit2.py
@@ -0,0 +1,30 @@
+# -*- coding=utf-8 -*-
+#@author:liuAmon
+#@contact:utopfish@163.com
+#@file:maxProfit2.py
+#@time: 2019/11/6 21:54
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)<=1:
+            return 0
+        diff = [0 for _ in range(len(prices)-1)]
+        for i in range(len(prices)-1):
+            diff[i] = prices[i+1]-prices[i]
+        dp = [0 for _ in range(len(prices)-1)]
+        dp[0] = max(0, diff[0])
+        max_profit = dp[0]
+        for i in range(1, len(prices)-1):
+            dp[i] = max(0, diff[i]+dp[i-1])
+            max_profit = max(max_profit, dp[i])
+        return max_profit
+class Solution2:
+    def maxProfit(self, prices: List[int]) -> int:
+        pro=0
+        for i in range(1,len(prices)):
+            temp=prices[i]-prices[i-1]
+            if temp>0:pro+=temp
+        return pro
+if __name__=="__main__":
+    Test=Solution()
+    print(Test.maxProfit([7,1,5,3,6,4]))
