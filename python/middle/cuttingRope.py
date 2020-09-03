94d96d542fe82d7feb8a5ffac7357bc0eaf77b6f
utopfish <utopfish@163.com>
2020-09-03 16:42:35 +0800

    01背包问题，晚上百度笔试

---
+++ python/middle/cuttingRope.py
@@ -0,0 +1,17 @@
+#@Time:2020/9/3 16:28
+#@Author:liuAmon
+#@e-mail:utopfish@163.com
+#@File:cuttingRope.py
+__author__ = "liuAmon"
+
+
+def cuttingRope(n: int) -> int:
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            tmp = max(j, dp[j]) * max(i - j, dp[i - j])
+            dp[i] = max(dp[i], tmp)
+    return dp[-1]
+
+
