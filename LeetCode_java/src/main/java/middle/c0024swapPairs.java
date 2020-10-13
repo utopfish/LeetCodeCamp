2bd7a056df4a49bd6ffa9b7abaec1f7e6a71d9a9
utopfish <utopfish@163.com>
2020-10-13 15:01:27 +0800

    <feature>:添加java编码方法

---
+++ LeetCode_java/src/main/java/middle/c0024swapPairs.java
@@ -0,0 +1,13 @@
+package middle;
+
+public class c0024swapPairs {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+}
